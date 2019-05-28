d6d28f50b3b1543b01008214f109ab1d71401a70
NatsuDzn <natsuxgraph@gmail.com>
2019-05-28 23:49:03 +0200

    playing with wowjs and animate

--- README.md
+++
@@ -1,2 +0,0 @@
-# portfolio-haha-yes
-wip 
--- js/script.js
+++ js/script.js
@@ -7,7 +7,6 @@ var one = "#000000",
 $(window).on("scroll touchmove", function() {
   if ($(document).scrollTop() >= $("#one").position().top) {
     $('body').css('background', $("#one").attr("data-color"));
-
   };
   if ($(document).scrollTop() > $("#two").position().top) {
     $('body').css('background', $("#two").attr("data-color"))
