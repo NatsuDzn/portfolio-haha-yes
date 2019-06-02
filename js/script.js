6236495d507c82e73ac5eebc24e422024939e1de
NatsuDzn <natsuxgraph@gmail.com>
2019-06-02 12:50:23 +0200

    remove useless stuff

--- js/script.js
+++ js/script.js
@@ -1,28 +1,4 @@
-var one = "#000000",
-  two = "#22a6b3",
-  three = "#0d78e7",
-  four = "#f88f18",
-  five = "#bb6774";
-
-$(window).on("scroll touchmove", function() {
-  if ($(document).scrollTop() >= $("#one").position().top) {
-    $('body').css('background', $("#one").attr("data-color"));
-  };
-  if ($(document).scrollTop() > $("#two").position().top) {
-    $('body').css('background', $("#two").attr("data-color"))
-  };
-  if ($(document).scrollTop() > $("#three").position().top) {
-
-    $('body').css('background', $("#three").attr("data-color"))
-  };
-  if ($(document).scrollTop() > $("#four").position().top) {
-
-    $('body').css('background', $("#four").attr("data-color"))
-  };
-  if ($(document).scrollTop() >= $("#five").position().top) {
-    $('body').css('background', $("#five").attr("data-color"))
-  };
-});
+ScrollReveal().reveal('.reveal');
 
 var TxtRotate = function(el, toRotate, period) {
   this.toRotate = toRotate;
